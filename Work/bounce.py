68439982af018d0234470b66cbf26a3a580a0d04
Arielle Dayva Scherr <arielle.scherr@austin.utexas.edu>
2023-11-21 16:37:29 -0600

    Added 3.18 solution in preparation for 4.1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
-# bounce.py
-#
-# Exercise 1.5
+initial_height = 100
+current_height = 100
+bouncing = 3/5
+bounce_ctr = 1
+
+while bounce_ctr <= 10:
+    current_height = round(current_height * bouncing, ndigits=4)
+    print(current_height)
+    bounce_ctr += 1
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,26 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 1
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
+        payment = payment + extra_payment
+    else:
+        payment = 2684.11
+
+    principal = round(principal * (1+rate/12) - payment, ndigits=2)
+    total_paid = round(total_paid + payment, ndigits=2)
+
+    print(f'{month} | {total_paid:0.2f} | {principal:0.2f}') 
+
+    month += 1
+
