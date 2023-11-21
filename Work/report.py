e5a38c5cc747a24f79e69c1b422e20ead885060e
Arielle Dayva Scherr <arielle.scherr@austin.utexas.edu>
2023-11-21 17:14:10 -0600

    Updated read_portfolio and make_report_data

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,7 @@
 # report.py
 
 import fileparse
+from stock import Stock
 
 def read_portfolio(filename):
     '''
@@ -8,7 +9,9 @@ def read_portfolio(filename):
     name, shares, and price.
     '''
     with open(filename) as lines:
-        return fileparse.parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
+        portdicts = fileparse.parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
+        portfolio = [Stock(d['name'], d['shares'], d['price']) for d in portdicts]
+        return portfolio
 
 def read_prices(filename):
     '''
@@ -24,9 +27,9 @@ def make_report_data(portfolio,prices):
     '''
     rows = []
     for stock in portfolio:
-        current_price = prices[stock['name']]
-        change = current_price - stock['price']
-        summary = (stock['name'], stock['shares'], current_price, change)
+        current_price = prices[stock.name]
+        change = current_price - stock.price
+        summary = (stock.name, stock.shares, current_price, change)
         rows.append(summary)
     return rows
 
